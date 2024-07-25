a4fdce6920e423edac5ef8358ccd043300cee2fc
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2024-07-25 23:28:44 +0200

    Proyecto Final

---
+++ Proyecto_Final/backend/contracts/Academy.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Academy {
+
+    enum CourseType {Cybersecurity, Development, ComputerNetworks, Blockchain}
+    string[] private courses = ["Cyber Security", "Development", "Computer Networks", "Blockchain"];
+    string[] private students;
+    mapping (CourseType => string[]) private academy;
+
+    constructor() { }
+    
+    function getCourses() public view returns (string[] memory) {
+        return courses;
+    }
+
+    function setStudentCourse(CourseType _course, string memory _studentName) public {
+        academy[_course].push(_studentName);
+        students.push(_studentName);
+    }
+
+    function getCourseStudents(CourseType _course) public view returns (string[] memory) {
+        return academy[_course];
+    }
+}
