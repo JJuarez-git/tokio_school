fbd26ec191bf185bbf5833b7a99f70c7309ecc49
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2024-07-25 11:21:44 +0200

    Proyecto Final

--- Proyecto_Final/backend/index.js
+++ Proyecto_Final/backend/index.js
@@ -1,9 +1,12 @@
-const Web3 = require("web3");
-const Web3Quorum = require("web3js-quorum");
-const web3 = new Web3Quorum(new Web3("http://localhost:8545"));
+const { Web3 } = require("web3");
 
-const wallet = web3.eth.accounts.wallet.create(1);
-console.log(wallet);
+const web3 = new Web3("http://localhost:8545");
+
+
+web3.eth.getNodeInfo().then(console.log).catch(console.error);
+
+/* const wallet = web3.eth.accounts.wallet.create(1);
+console.log(wallet); */
 
 // console.log(web3.eth.accounts.wallet);
 
@@ -11,5 +14,5 @@ console.log(wallet);
 
 // web3.eth.accounts.wallet.add(account);
 
-web3.eth.getAccounts().then(console.log);
-// web3.eth.getBlock(1).then(console.log);
+// web3.eth.getAccounts().then(console.log);
+// web3.eth.getBlock().then(console.log);
