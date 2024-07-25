dc5452d3b0a292689e8b70db8cd020fcf6aa7d8c
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2024-07-25 19:56:24 +0200

    Proyecto Final

--- Proyecto_Final/backend/src/index.ts
+++ Proyecto_Final/backend/src/index.ts
@@ -1,14 +1,14 @@
-const { Web3 } = require("web3");
+import { Web3 } from "web3";
 
 (async () => {
     const web3 = new Web3("http://127.0.0.1:8545");
     
     try {
-        // const block = await web3.eth.getBlock();
-        // console.log("Block", block);
+        const block = await web3.eth.getBlock();
+        console.log("Block", block);
 
         const accounts = await web3.eth.getAccounts();
-        console.log(accounts);
+        console.log("Accounts", accounts);
 
     } catch (error) {
         console.error(error)   
