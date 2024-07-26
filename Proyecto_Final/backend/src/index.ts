6110e4039b1158b1f1ea9eb798865ddb2e8a28d9
José Carlos Juárez <64013592+JJuarez-git@users.noreply.github.com>
2024-07-26 10:24:29 +0200

    Update index.ts

--- Proyecto_Final/backend/src/index.ts
+++ Proyecto_Final/backend/src/index.ts
@@ -2,13 +2,26 @@ import { Web3 } from "web3";
 
 (async () => {
     const web3 = new Web3("http://127.0.0.1:8545");
-    
-    try {
-        const block = await web3.eth.getBlock();
-        console.log("Block", block);
+    const AcademyJSON = require("../artifacts/contracts/Academy.sol/Academy.json");
+    const deployedAddresses = require("../ignition/deployments/chain-1337/deployed_addresses.json");
 
-        const accounts = await web3.eth.getAccounts();
-        console.log("Accounts", accounts);
+    try {
+        const AcademyContract = new web3.eth.Contract(
+            AcademyJSON.abi,
+            deployedAddresses['AcademyModule#Academy']
+        );
+
+        const courses: string[] = await AcademyContract.methods.getCourses().call();
+        const map = new Map();
+
+        courses.forEach((v, i) => map.set(i, v));
+        console.log("CURSOS", map);
+
+        /* const tx = await web3.eth.getTransaction("0x0b039cb47ca05cc36b106c456149f494e20a33ce60d69016b4b8cf502af688ce");
+        console.log("TX", tx); */
+
+        /* const accounts = await web3.eth.getAccounts();
+        console.log("Accounts", accounts); */
 
     } catch (error) {
         console.error(error)   
