e6d0e73644fac55e3f7e23230053d9da808d32ae
José Carlos Juárez <64013592+JJuarez-git@users.noreply.github.com>
2024-07-26 10:52:20 +0200

    Update index.ts

--- Proyecto_Final/backend/src/index.ts
+++ Proyecto_Final/backend/src/index.ts
@@ -1,30 +1,29 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { Web3 } from "web3";
 
-(async () => {
-    const web3 = new Web3("http://127.0.0.1:8545");
-    const AcademyJSON = require("../artifacts/contracts/Academy.sol/Academy.json");
-    const deployedAddresses = require("../ignition/deployments/chain-1337/deployed_addresses.json");
+const app = express();
+const port = 4000;
+app.use(cors());
+app.use(bodyParser.json());
 
+const web3 = new Web3("http://127.0.0.1:8545");
+
+const AcademyJSON = require("../artifacts/contracts/Academy.sol/Academy.json");
+const deployedAddresses = require("../ignition/deployments/chain-1337/deployed_addresses.json");
+const AcademyContract = new web3.eth.Contract(AcademyJSON.abi, deployedAddresses['AcademyModule#Academy']);
+
+app.get('/courses', async (req, res) => {
     try {
-        const AcademyContract = new web3.eth.Contract(
-            AcademyJSON.abi,
-            deployedAddresses['AcademyModule#Academy']
-        );
+        const response: string[] = await AcademyContract.methods.getCourses().call();
+        res.status(200).json(response);
 
-        const courses: string[] = await AcademyContract.methods.getCourses().call();
-        const map = new Map();
-
-        courses.forEach((v, i) => map.set(i, v));
-        console.log("CURSOS", map);
-
-        /* const tx = await web3.eth.getTransaction("0x0b039cb47ca05cc36b106c456149f494e20a33ce60d69016b4b8cf502af688ce");
-        console.log("TX", tx); */
-
-        /* const accounts = await web3.eth.getAccounts();
-        console.log("Accounts", accounts); */
-
-    } catch (error) {
-        console.error(error)   
+    } catch (error: any) {
+        res.status(500).send(error.message);
     }
-})();
+});
 
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
