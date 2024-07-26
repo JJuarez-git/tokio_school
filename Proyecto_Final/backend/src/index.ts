32c2e513a716fd8ac8aafdb9b26300a271f39a40
José Carlos Juárez <64013592+JJuarez-git@users.noreply.github.com>
2024-07-26 14:07:41 +0200

    Update index.ts

--- Proyecto_Final/backend/src/index.ts
+++ Proyecto_Final/backend/src/index.ts
@@ -24,6 +24,41 @@ app.get('/courses', async (req, res) => {
     }
 });
 
+app.get('/students', async (req, res) => {
+    try {
+        const response: string[] = await AcademyContract.methods.getStudents().call();
+        res.status(200).json(response);
+
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post('/course/student', async (req, res) => {
+    try {
+        const { sender, course, student } = req.body;
+        const response = await AcademyContract.methods.setStudentCourse(Number(course), student).send({
+            from: sender,
+            gas: '1000000'
+        });
+        res.status(200).json({ tx: response.transactionHash });
+
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.get('/course/:course/students', async (req, res) => {
+    try {
+        const { course } = req.params;
+        const response: string[] = await AcademyContract.methods.getCourseStudents(Number(course)).call();
+        res.status(200).json(response);
+
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
