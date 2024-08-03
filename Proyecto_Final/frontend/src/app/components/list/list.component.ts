60372a1c3f769ec1f3ba972ad12a3125d3e3aecd
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2024-08-03 19:48:17 +0200

    Proyecto Final

--- Proyecto_Final/frontend/src/app/components/list/list.component.ts
+++ Proyecto_Final/frontend/src/app/components/list/list.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  students: string[] = ["Jose", "Marta", "Luis", "Natalia"];
+
+  avatarURL(name: string) {
+    return `https://api.multiavatar.com/${name}.svg`;
+  }
 
 }
